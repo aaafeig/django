5def4285d5d1bcedf8b875f1d455844264d5a366
aaafeig <geroy.ozhog08@mail.ru>
2025-11-09 20:12:06 +0300

    Выполнил 1 задание: создал контроллер, настроил маршутизатор и создал шаблон для отображения информации о продукте

--- catalog/models.py
+++ catalog/models.py
@@ -21,7 +21,7 @@ class Category(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=150, verbose_name = 'Название')
     description = models.TextField(verbose_name = 'Описание')
-    image = models.ImageField(verbose_name = 'Изображение', null=True)
+    image = models.ImageField(verbose_name = 'Изображение', null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name = 'категория')
     price = models.IntegerField(verbose_name = 'Цена')
     updated_at = models.DateField(auto_now=True)
