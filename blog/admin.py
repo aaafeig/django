ce33b3d82c93c7604b06c675bd4dfece8cd6fd94
aaafeig <geroy.ozhog08@mail.ru>
2025-11-10 23:49:53 +0300

    Выполнил 3 задание: добавил простой счетчик просмтров, кнопку для фильтрации опубликованных и неопубликованных блогов и перенаправление пользователя на страницу деталей блога после редактирования

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,6 @@ from .models import Post
 
 @admin.register(Post)
 class AdminPost(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_published', 'created_at', 'preview')
+    list_display = ('id', 'title', 'is_published', 'created_at', 'preview', 'views_count')
     list_filter = ('is_published',)
     search_fields = ('title', 'content',)
--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,28 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        if self.request.GET.get('published') == 'true':
+            queryset = queryset.filter(is_published=True)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_published_filter'] = self.request.GET.get('published') == 'true'
+        return context
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        post = super().get_object(queryset)
+        post.views_count += 1
+        post.save()
+        return post
+
 
 class PostCreateView(CreateView):
     model = Post
@@ -26,7 +44,10 @@ class PostUpdateView(UpdateView):
     model = Post
     fields = ['title', 'content', 'preview']
     template_name = 'blog/post_update.html'
-    success_url = reverse_lazy('blog:post_list')
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+
 
 class PostDeleteView(DeleteView):
     model = Post
