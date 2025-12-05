81a75b12342e7c685cf6d8e8e68765d62ff28db9
aaafeig <geroy.ozhog08@mail.ru>
2025-12-06 00:04:22 +0300

    Исправил ошибки и недоработки

--- catalog/forms.py
+++ catalog/forms.py
@@ -37,14 +37,14 @@ class ProductForm(forms.ModelForm):
     def clean_title(self):
         title = self.cleaned_data.get('title')
         ban_words = ['казино','криптовалюта','крипта','биржа','дешево','бесплатно','обман','полиция','радар']
-        if any(title in word for word in ban_words):
+        if any(ban_word in title.lower() for ban_word in ban_words):
             raise ValidationError('В название есть запрещенные слова')
         return title
 
     def clean_description(self):
         description = self.cleaned_data.get('description')
         ban_words = ['казино','криптовалюта','крипта','биржа','дешево','бесплатно','обман','полиция','радар']
-        if any(description in word for word in ban_words):
+        if any(ban_word in description.lower() for ban_word in ban_words):
             raise ValidationError('В описание есть запрещенные слова')
         return description
 
